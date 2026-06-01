c31a0da5aefdc6d118d6bae901da854987d68f4a
Jon <jonmd@hotmail.com>
2026-06-01 03:52:00 -0500

    Added basic FastAPI endpoint and dynamic version tracking

---
+++ src/overwatch_hero_stats/__init__.py
@@ -0,0 +1,4 @@
+from importlib.metadata import version
+
+module_name = "overwatch_hero_stats"
+__version__ = version(module_name)
--- src/overwatch_hero_stats/__main__.py
+++ src/overwatch_hero_stats/__main__.py
@@ -1,3 +1,5 @@
 from .app_main import main_app
+import uvicorn
 
 main_app.start()
+uvicorn.run("overwatch_hero_stats.api_main:app", host="0.0.0.0", port=8000, reload=True)
