29a2e7309ab08c01a3860ace5f2eb265250a402d
Jon <jonmd@hotmail.com>
2026-06-01 06:19:51 -0500

    Genericize test data

--- src/overwatch_hero_stats/app_main.py
+++ src/overwatch_hero_stats/app_main.py
@@ -5,20 +5,26 @@ from .data_fetcher.hero_fetcher import HeroDataFetcher
 from .data_fetcher.hero_filters import HeroFilters, HFType
 
 #TEST
+from .data_fetcher.hero_options.hero_input import HeroInput
 from .data_fetcher.hero_options.hero_map import HeroMap
 from .data_fetcher.hero_options.hero_tier import HeroTier
-from .data_fetcher.hero_options.hero_input import HeroInput
 from .data_fetcher.hero_options.hero_queue_type import HeroQueueType
 from .data_fetcher.hero_options.hero_region import HeroRegion
 # END
 
 
-def test_filters(herofilters: HeroFilters, map: HeroMap = HeroMap.circuit_royal.value):
-    herofilters.set_filter_value(HFType.INPUT, HeroInput.PC.value)
+def test_filters(herofilters: HeroFilters,
+        input: int = HeroInput.PC.value,
+        map: int = HeroMap.all_maps.value,
+        tier: int = HeroTier.All.value,
+        queue_type = HeroQueueType.COMP_Role_Queue.value,
+        region = HeroRegion.Americas.value,
+    ):
+    herofilters.set_filter_value(HFType.INPUT, input)
     herofilters.set_filter_value(HFType.MAP, map)
-    herofilters.set_filter_value(HFType.TIER, HeroTier.Silver.value)
-    herofilters.set_filter_value(HFType.QUEUE_TYPE, HeroQueueType.COMP_Role_Queue.value)
-    herofilters.set_filter_value(HFType.REGION, HeroRegion.Americas.value)
+    herofilters.set_filter_value(HFType.TIER, tier)
+    herofilters.set_filter_value(HFType.QUEUE_TYPE, queue_type)
+    herofilters.set_filter_value(HFType.REGION, region)
 
 current_roster = [
             "soldier-76",
@@ -35,10 +41,11 @@ class main_app:
         herofilters = HeroFilters()
         test_filters(herofilters)
         fetcher = HeroDataFetcher()
-        # hero_dict = fetcher.get_hero_dict()
+        
+        full_roster = fetcher.get_hero_dict().keys()
 
         for map in HeroMap.__members__:
-            test_filters(herofilters, HeroMap[map].value)
+            test_filters(herofilters=herofilters, tier=HeroTier.Silver.value, map=HeroMap[map].value)
             hero_data = fetcher.fetch_data(herofilters)
-            best_heroes = fetcher.get_best_hero(hero_ids=current_roster, data=hero_data, count=3)
+            best_heroes = fetcher.get_best_hero(hero_ids=full_roster, data=hero_data, count=1)
             logger.debug(f"Best Heroes ({map}): {best_heroes}")
--- src/overwatch_hero_stats/data_fetcher/hero_fetcher.py
+++ src/overwatch_hero_stats/data_fetcher/hero_fetcher.py
@@ -25,7 +25,7 @@ class HeroDataFetcher:
         hero_dict :dict = {}
 
         url = self._assemble_URL(herofilters)
-        self.logger.debug(f"Fetching from URL:{url}")
+        # self.logger.debug(f"Fetching from URL:{url}")
 
         if TEST_MODE:
             self.logger.debug("Fake Data")
